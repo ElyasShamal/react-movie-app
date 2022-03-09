15008be97ddaa2f7d1c8bfc4da35747e56af28d0
ElyasShamal <89407707+ElyasShamal@users.noreply.github.com>
2022-03-09 18:01:29 -0500

    Movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from "react";
+
+import Movie from './components/Movie';
+
+
+
+const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+
+const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [ movies, setMovies ] = useState([]); 
+
+const [searchTerm, setSearchTerm] = useState('');
+
+
+  useEffect(()  => {
+    fetch(FEATURED_API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  }, []);
+    
+
+   const handleOnSubmit = (e) => {
+         e.preventDefault();
+
+         if(searchTerm){
+         fetch(SEARCH_API + searchTerm)
+         .then((res) => res.json())
+         .then((data) => {
+           console.log(data);
+           setMovies(data.results);
+         });
+
+         setSearchTerm('');
+        }
+   };
+
+
+   const handeOnChange = (e) => {
+     setSearchTerm(e.target.value);
+   }
+
+  return ( 
+   
+  <div>
+    
+    <header>
+       <form onSubmit={handleOnSubmit}>
+         <input 
+           classname="search"
+             type="text" 
+              placeholder="Search ..."
+                Value={searchTerm}
+                onChange={handeOnChange}
+            ></input>
+      
+      </form>
+    
+  </header> 
+
+  <div className="movie-container">
+    {movies.length > 0 && movies.map((movie) => 
+      <Movie key={movie.id} {...movie} />)}
+  </div>
+  </div>
+  )
 }
 
 export default App;
